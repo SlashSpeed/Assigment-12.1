7893fa1e1cfb881cd0e4b516766db8071835b84d
Slash <michael.breznik@gmail.com>
2016-11-10 19:04:51 +0100

    added a model

--- Assigment 12.1.py
+++ Assigment 12.1.py
@@ -10,6 +10,7 @@ soup = BeautifulSoup(response)
 email_seznam = []
 seznam_mest =[]
 seznam_imen = []
+seznam_oseb = []
 
 for link in soup.findAll("a"):
     if link.string == "See full profile":
@@ -24,11 +25,23 @@ for link in soup.findAll("a"):
         seznam_mest.append(str(city.string))
         seznam_imen.append(str(name_surname.string))
 
+#model oz. class za zdruzitev vseh treh seznamov
+class Contact:
+    def __init__(self, email, city, name_surname):
+        self.email = email
+        self.city = city
+        self.name_surname =name_surname
+
+#dodamo vse tri stringe v kontakt osebe
+oseba = Contact(email=email, city=city, name_surname=name_surname)
+seznam_oseb.append(oseba)
+
+print (seznam_oseb)
+
 
 csv_file = open("data_scraper.csv", "w")
-csv_file.write(str(email_seznam) + ", " + "\n")
-csv_file.write(str(seznam_mest) + ", " + "\n")
-csv_file.write(str(seznam_imen) + ", " + "\n")
+csv_file.write(str(seznam_oseb) + ", " + "\n")
+
 
 
 csv_file.close()
